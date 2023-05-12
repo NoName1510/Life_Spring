9edb7bd0f04e6364e64aa81726a9692afad67542
huygm1510 <tranconghuyhuy.gm@gmail.com>
2023-05-12 21:34:00 +0700

    change show

--- src/main/java/org/example/ConfigBean.java
+++ src/main/java/org/example/ConfigBean.java
@@ -1,2 +1,13 @@
-package org.example;public class ConfigBean {
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigBean {
+    @Bean
+    public MyBean myBean(){
+        return new MyBean();
+    }
+
 }
--- src/main/java/org/example/MyBean.java
+++ src/main/java/org/example/MyBean.java
@@ -1,2 +1,19 @@
-package org.example;public class MyBean {
+package org.example;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class MyBean {
+    @PostConstruct
+    public void init(){
+        System.out.println("run new");
+    }
+    @PreDestroy
+    public void destroy(){
+        System.out.println("end");
+    }
+
+    public void show(){
+        System.out.println("Cô Loan Xinh Gái =))");
+    }
 }
